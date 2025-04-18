ef649d34c2479f5a5401ae04680dcfdd5cfc7c5c
git-alexis <alex59.billemont@gmail.com>
2025-04-18 23:22:09 +0200

    Add Island Perimeter algorithm

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"Function that calculates the perimeter of an island in a grid"
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    height = len(grid)
+    width = len(grid[0])
+
+    for indexRow in range(height):
+        for indexColumn in range(width):
+            if grid[indexRow][indexColumn] == 1:
+                if indexRow == 0 or grid[indexRow - 1][indexColumn] == 0:
+                    perimeter += 1
+                if indexRow == height - 1 or grid[indexRow + 1][indexColumn] == 0:
+                    perimeter += 1
+                if indexColumn == 0 or grid[indexRow][indexColumn - 1] == 0:
+                    perimeter += 1
+                if indexColumn == width - 1 or grid[indexRow][indexColumn + 1] == 0:
+                    perimeter += 1
+
+    return perimeter

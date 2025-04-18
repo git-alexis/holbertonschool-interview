2e17a12b8ac4cefb9db0bd35e67ea6714edf7c34
git-alexis <alex59.billemont@gmail.com>
2025-04-18 18:27:05 +0200

    Fix PEP 8 style problems

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"Method that determines if all the boxes can be opened"
+
+
 def canUnlockAll(boxes):
 	visited = set()
-	stack = [0]
+	keysStack = [0]
 
-	while stack:
-		box_index = stack.pop()
+	while keysStack:
+		box_index = keysStack.pop()
 		if box_index not in visited:
 			visited.add(box_index)
 			for key in boxes[box_index]:
 				if key < len(boxes):
-					stack.append(key)
+					keysStack.append(key)
 
 	return len(visited) == len(boxes)

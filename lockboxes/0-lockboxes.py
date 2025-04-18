e80f4fec8e4e95d429ec431a1f990e239d7ec5f4
git-alexis <alex59.billemont@gmail.com>
2025-04-18 18:28:14 +0200

    Fix PEP 8 style identation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,15 +3,15 @@
 
 
 def canUnlockAll(boxes):
-	visited = set()
-	keysStack = [0]
+    visited = set()
+    keysStack = [0]
 
-	while keysStack:
-		box_index = keysStack.pop()
-		if box_index not in visited:
-			visited.add(box_index)
-			for key in boxes[box_index]:
-				if key < len(boxes):
-					keysStack.append(key)
+    while keysStack:
+        box_index = keysStack.pop()
+        if box_index not in visited:
+            visited.add(box_index)
+            for key in boxes[box_index]:
+                if key < len(boxes):
+                    keysStack.append(key)
 
-	return len(visited) == len(boxes)
+    return len(visited) == len(boxes)
